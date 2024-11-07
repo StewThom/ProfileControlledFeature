a3d02699c47dfa90478c434950dc7851e55e555c
Stewart Thomson <Stew12Thom@gmail.com>
2024-11-08 10:34:50 +1100

    Initial Commit

---
+++ ProfileControlledFeature/ProfileControlledFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileControlledFeatureApp.swift
+//  ProfileControlledFeature
+//
+//  Created by Stewart Thomson on 8/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProfileControlledFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProfileControlledFeatureTests/ProfileControlledFeatureTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileControlledFeatureTests.swift
+//  ProfileControlledFeatureTests
+//
+//  Created by Stewart Thomson on 8/11/2024.
+//
+
+import Testing
+@testable import ProfileControlledFeature
+
+struct ProfileControlledFeatureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
